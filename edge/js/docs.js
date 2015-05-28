dcc3cf3091553b81d9e2b15e87adc1c8b92bcfa1
cfh463 <rob.galvin@motorolasolutions.com>
2015-05-28 14:47:01 -0400

    updated GA code

--- edge/js/docs.js
+++ edge/js/docs.js
@@ -142,10 +142,15 @@ function GA(hash){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
+  version = window.location.pathname.split('/')[2];
   ga('create', 'UA-63523200-1', 'auto');
   ga('send', 'pageview', hash);
-  console.log('sending stats for ' + hash);
+  ga('send', 'screenview', {
+	  'appName': 'EMDK Docs',
+	  'appVersion': version,
+	  'screenName': hash
+  });
+  console.log('sending stats for ' + hash + ":" + version);
 }
 
 
